46111134a43684b5fb59eec0e2e764f399ee2219
Naveen Kuratti <naveenkuratti875@gmail.com>
2024-07-22 17:25:27 +0530

    Your commit message here

---
+++ h
@@ -0,0 +1,11 @@
+[33mcommit f02ea0f6de0b8711a56152136e4a077f49a4c05c[m[33m ([m[1;36mHEAD[m[33m -> [m[1;32mmain[m[33m, [m[1;31morigin/main[m[33m)[m
+Author: Naveen Kuratti <naveenkuratti875@gmail.com>
+Date:   Mon Jul 22 17:18:57 2024 +0530
+
+    first commit
+
+[33mcommit a069f29df061b9a5fabf20fed53492a24629ee69[m
+Author: Naveen Kuratti <naveenkuratti875@gmail.com>
+Date:   Mon Jul 22 17:16:56 2024 +0530
+
+    first commit
---
+++ src/main/java/com/uniquedeveloper/registration/RegistrationServlet.java
@@ -0,0 +1,30 @@
+package com.uniquedeveloper.registration;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/register")
+public class RegistrationServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String uname = request.getParameter("name");
+        String uemail = request.getParameter("email");
+        String upwd = request.getParameter("pass");
+        String umobile = request.getParameter("contact");
+        PrintWriter out=response.getWriter();
+        out.print(uname);
+        out.print(uemail);
+        out.print(upwd);
+        out.print(umobile);
+    }
+}
